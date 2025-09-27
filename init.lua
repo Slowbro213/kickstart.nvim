b2882dfbfeef0a2ef4e1fb524a9c25162b8f29ba
Slowbro213 <thanas.papa.24@gmail.com>
2025-09-27 13:30:42 +0200

    cleanup

--- init.lua
+++ init.lua
@@ -1,25 +1,14 @@
--- Set <space> as the leader key
--- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
-
--- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
--- [[ Setting options ]]
--- See `:help vim.o`
--- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
-
 require 'custom.options'
 
--- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
-
 require 'custom.keymaps'
 
--- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
 -- Highlight when yanking (copying) text
